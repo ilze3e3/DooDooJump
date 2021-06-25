7fb6dbe25589f276431e68189319d1c15c57e5f3
ilze3e3 <78832179+ilze3e3@users.noreply.github.com>
2021-06-26 01:37:42 +1000

    Added audio settings

--- Assets/Scripts/MenuScripts/OptionsHandler.cs
+++ Assets/Scripts/MenuScripts/OptionsHandler.cs
@@ -10,7 +10,32 @@ public class OptionsHandler : MonoBehaviour
     float currentMasterVol;
     float currentMusicVol;
     float currentSFXVol;
+    bool isMuted;
 
+    [SerializeField]Slider masterSlider;
+    [SerializeField]Slider musicSlider;
+    [SerializeField]Slider sfxSlider;
+    [SerializeField]Toggle muteToggle;
+
+    private void Start()
+    {
+        currentMasterVol = PlayerPrefs.GetFloat("MasterVolume", 0);
+        currentMusicVol = PlayerPrefs.GetFloat("MusicVolume", 0);
+        currentSFXVol = PlayerPrefs.GetFloat("SFXVolume", 0);
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            isMuted = false;
+        }
+        else
+        {
+            isMuted = true;
+        }
+
+        masterSlider.value = currentMasterVol;
+        musicSlider.value = currentMusicVol;
+        sfxSlider.value = currentSFXVol;
+        muteToggle.isOn = isMuted;
+    }
     public void ChangeMasterVolume(float _vol)
     {
         currentMasterVol = _vol;
@@ -31,6 +56,7 @@ public class OptionsHandler : MonoBehaviour
 
     public void ToggleMuteMasterVolume(bool _mute)
     {
+        isMuted = _mute;
         if(_mute)
         {
             masterMixer.SetFloat("MasterVolume", -80.0f);
@@ -39,5 +65,14 @@ public class OptionsHandler : MonoBehaviour
         {
             masterMixer.SetFloat("MasterVolume", currentMasterVol);
         }
+    }
+
+    public void SaveAudioSettings()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", currentMasterVol);
+        PlayerPrefs.SetFloat("MusicVolume", currentMusicVol);
+        PlayerPrefs.SetFloat("SFXVolume", currentSFXVol);
+        if (isMuted) PlayerPrefs.SetInt("Mute", 1);
+        else PlayerPrefs.SetInt("Mute", 0);
     }
 }
